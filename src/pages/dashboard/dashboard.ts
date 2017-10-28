53ad8ed9700b7c053cd9db266151cfca47e675ad
reverie <ss.saswatsahoo@gmail.com>
2017-10-28 18:46:01 +0530

    Dashboard page added

---
+++ src/pages/dashboard/dashboard.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { TabsPage } from '../tabs/tabs';
+
+/**
+ * Generated class for the DashboardPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-dashboard',
+  templateUrl: 'dashboard.html',
+})
+export class DashboardPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad DashboardPage');
+  }
+  	hospitals(){
+		console.log("Hospitals buton Clicked");
+		this.navCtrl.push(TabsPage);
+
+  }
+}
